15854532f04cefa0d92562dad321d577486196ad
Lyz03 <lizoe@netcourrier.com>
2021-11-10 16:36:14 +0100

    exo 53

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let a = 0;
 
 while (a<span.length){
     span[a].style.backgroundColor = "gray";
+    span[a].dir = "rtl";
     a++;
 }
 
@@ -25,16 +26,24 @@ while (b<animatedParagraphe.length){
 }
 
 let lien1 = document.getElementById('lien1');
-lien1.innerText = "lien1"
+lien1.innerText = "lien1";
+lien1.href = "#";
 
 let lien2 = document.getElementById('lien2');
-lien2.innerText = "lien2"
+lien2.innerText = "lien2";
+lien2.href = "#";
 
 let lien3 = document.getElementById('lien3');
-lien3.innerText = "lien3"
+lien3.innerText = "lien3";
+lien3.href = "#";
 
 let lien4 = document.getElementById('lien4');
-lien4.innerText = "lien4"
+lien4.innerText = "lien4";
+lien4.href = "#";
 
 let lien5 = document.getElementById('lien5');
-lien5.innerText = "lien5"
+lien5.innerText = "lien5";
+lien5.href = "#";
+
+let lastParagraphe = document.getElementById('last_paragraphe');
+lastParagraphe.innerText = "le nouveau text du dernier paragraphe";
